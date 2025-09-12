5a67c3b303163c8604416e1cbcc1f9433417bb17
mestylesolution.dev <mestylesolution.dev@gmail.co>
2025-09-13 01:36:55 +0700

    fix: remove unnecessary districtCode dependency from province effect hook

--- app/components/forms/TaxInvoiceForm.tsx
+++ app/components/forms/TaxInvoiceForm.tsx
@@ -529,7 +529,7 @@ export default function TaxInvoiceForm() {
     return () => {
       mounted = false
     }
-  }, [formData.provinceCode, formData.districtCode])
+  }, [formData.provinceCode])
 
   // When district changes, load subdistricts
   useEffect(() => {
